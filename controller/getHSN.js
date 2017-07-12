324e6dbeb17f283c19f0cb797e33ca7646d24e7f
tabish.khan <tabish.khan@tothenew.com>
2017-07-12 15:21:51 +0530

    added the pagination functionality

--- controller/getHSN.js
+++ controller/getHSN.js
@@ -3,12 +3,15 @@ const hsn = require('./../db/schema/gethsn');
 
 module.exports = {
   get:function(req, res){
+    var limit = parseInt(req.query.limit);
+    var skip = (parseInt(req.query.skip)-1) * parseInt(limit);
+    console.log("{{{{{{{{{{{}}}}}}}}}}}", limit, skip)
       db.on('connected',()=> {console.log('MongoDB Connected')});
             hsn.find({},(err, data)=>{
              if (err)
                 throw err;
              console.log('HSN fetched successfully!');
       res.status(200).send({data:data});
-    })
+    }).skip(skip).limit(limit)
   }
 }
