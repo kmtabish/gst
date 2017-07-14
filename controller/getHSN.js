e16b0b43d9895a7cded87019fabed6d319539fe8
tabish.khan <tabish.khan@tothenew.com>
2017-07-14 14:57:49 +0530

    added cors

--- controller/getHSN.js
+++ controller/getHSN.js
@@ -5,6 +5,11 @@ module.exports = {
   get:function(req, res){
     var limit = parseInt(req.query.limit);
     var skip = (parseInt(req.query.skip)-1) * parseInt(limit);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,contenttype'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
     console.log("{{{{{{{{{{{}}}}}}}}}}}", limit, skip)
       db.on('connected',()=> {console.log('MongoDB Connected')});
             hsn.find({},(err, data)=>{
@@ -13,5 +18,6 @@ module.exports = {
              console.log('HSN fetched successfully!');
       res.status(200).send({data:data});
     }).skip(skip).limit(limit)
+
   }
 }
