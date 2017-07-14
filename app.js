e19ff5413a462a13164cd503d77b15b4b9514ab8
tabish.khan <tabish.khan@tothenew.com>
2017-07-14 15:30:23 +0530

    fixed cors issue

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const PORT = process.env.PORT || 1234;
 // get all data/stuff of the body (POST) parameters
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/bower_components', express.static(path.join(__dirname, 'bower_components')));
